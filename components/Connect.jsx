78ab400bd5611e281e2ce49ff8696bf7779872ba
ej.glng <egalang7@my.bcit.ca>
2026-05-04 16:11:03 -0700

    initial structure

---
+++ components/Connect.jsx
@@ -0,0 +1,21 @@
+"use react"
+
+import { useState, useEffect, useRef } from 'react'
+
+export default function Connect(){
+    return(
+        // create profiles
+
+        // create melody room button
+
+        // add arrow animation
+
+        // add music player
+
+        // create song queue
+
+        // create chat/messages
+
+        // add leave melody room button (resets)
+    );
+}
