2d89d56cd2d6607b2f79e17e2b0df64ff3f09305
Sofiia <smoskalets@my.bcit.ca>
2026-05-10 20:05:58 -0700

    fix hover, click, lines, images functionality

--- components/Listen.jsx
+++ components/Listen.jsx
@@ -5,9 +5,26 @@ import styles from "./Listen.module.css";
 export default function Listen() {
 
 const [isWiggling, setIsWiggling] = useState(false);
-const [isActive, setIsActive] = useState(false);
+const [showLines, setShowLines] = useState(false);
+const [showImages, setShowImages] = useState(false);
 
-console.log("styles:", styles);
+
+function hoverWiggle(state) {
+  setIsWiggling(state);
+}
+
+useEffect(() => {
+  if (showLines) {
+    const timer = setTimeout(() => {
+      setShowImages(true);
+    }, 500);
+    return () => clearTimeout(timer);
+  } else {
+    setShowImages(false);
+  }
+}, [showLines]);
+
+
   return (
     <div className={styles.page}>
         {/* Title */}
@@ -17,26 +34,26 @@ console.log("styles:", styles);
 
       {/* Image */}
       <img src="/elodie.svg" className={`${styles.mainImg} ${isWiggling ? styles.wiggle: ""}`}
-      onMouseEnter={()=>{console.log("hover"); setIsWiggling(true)}}
-      onMouseLeave={()=>{console.log("leave");setIsWiggling(false)}}
-      onClick={()=>{console.log("click");setIsActive(true)}} />
+      onMouseEnter={()=>{hoverWiggle(true)}}
+      onMouseLeave={()=>{hoverWiggle(false)}}
+      onClick={()=>{setShowLines(true)}} />
 
       {/* dashed lines */}
       <div className={styles.lines}>
-        <div className={`${styles.line} ${styles.lineTopRight} ${isActive ? styles.showLine:""}`}></div>
-        <div className={`${styles.line} ${styles.lineTopLeft} ${isActive ? styles.showLine:""}`}></div>
-        <div className={`${styles.line} ${styles.lineBottomRight} ${isActive ? styles.showLine:""}`}></div>
-        <div className={`${styles.line} ${styles.lineBottomLeft} ${isActive ? styles.showLine:""}`}></div>
+        <div className={`${styles.line} ${styles.lineTopRight} ${showLines ? styles.showLine:""}`}></div>
+        <div className={`${styles.line} ${styles.lineTopLeft} ${showLines ? styles.showLine:""}`}></div>
+        <div className={`${styles.line} ${styles.lineBottomRight} ${showLines ? styles.showLine:""}`}></div>
+        <div className={`${styles.line} ${styles.lineBottomLeft} ${showLines ? styles.showLine:""}`}></div>
+      </div>
+
+      {/* 4 smaller images */}
+      <div className={styles.images}>
+        <img src="/topright.svg" className={`${styles.smallImg} ${styles.imgTopRight} ${showImages ? styles.showImg:""}`}></img>
+        <img src="/topleft.svg" className={`${styles.smallImg} ${styles.imgTopLeft} ${showImages ? styles.showImg:""}`}></img>
+        <img src="/bottomright.svg" className={`${styles.smallImg} ${styles.imgBottomRight} ${showImages ? styles.showImg:""}`}></img>
+        <img src="/bottomleft.svg" className={`${styles.smallImg} ${styles.imgBottomLeft} ${showImages ? styles.showImg:""}`}></img>
       </div>
      </div>
-
-{/* 4 smaller images */}
-      <div className={styles.images}>
-        <img src="/topright.svg" className={`${styles.smallImg} ${styles.imgTopRight} ${isActive ? styles.showImg:""}`}></img>
-        <img src="/topleft.svg" className={`${styles.smallImg} ${styles.imgTopLeft} ${isActive ? styles.showImg:""}`}></img>
-        <img src="/bottomright.svg" className={`${styles.smallImg} ${styles.imgBottomRight} ${isActive ? styles.showImg:""}`}></img>
-        <img src="/bottomleft.svg" className={`${styles.smallImg} ${styles.imgBottomLeft} ${isActive ? styles.showImg:""}`}></img>
-      </div>
 
 </div>
   );
